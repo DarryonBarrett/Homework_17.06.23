89b8d92fbecb8b733e9697873e0967d27c8aa945
DBarrett <darryonbarrett@gmail.com>
2023-06-21 21:55:31 +0300

    Finished work on Ex_001

---
+++ Ex_001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int InputNum()
+{
+    Console.Write("Введите пятизначное число: ");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void FindPalind(int num)
+{
+    if ((num / 10000) == (num % 10) & (((num % 10) %10) %10) == ((num % 10)%10))
+    {
+        Console.WriteLine("Данное число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Данное число не является палиндромом");
+    }
+}
+
+int number = InputNum();
+FindPalind(number);
