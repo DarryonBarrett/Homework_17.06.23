505525dbd4246834dcd7a765831f053c146d1727
DBarrett <darryonbarrett@gmail.com>
2023-06-21 22:09:27 +0300

    Finished work on Ex_003

---
+++ Ex_003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+     return num;
+}
+
+int num = InputNum("Введите некоторое число N:");
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}");
+    if (i != num)
+    System.Console.Write(", ");
+    else
+    System.Console.Write(".");
+}
